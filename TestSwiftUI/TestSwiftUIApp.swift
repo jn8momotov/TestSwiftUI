3da36f25ff0d6b19373d29d600cc51edd108ede1
Evgeniy Momotov <jn8momotov@gmail.com>
2020-12-03 19:17:09 +0300

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by Момотов Евгений Олегович on 03.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
